fe286c50b42034df968b21b0fc53f20866b54537
SamaniegoM <dsamaniegom@edu.tecnocampus.cat>
2025-11-03 16:42:02 +0100

    portals working

--- Assets/Code/Portal.cs
+++ Assets/Code/Portal.cs
@@ -9,7 +9,7 @@ public class Portal : MonoBehaviour
     public Portal otherPortal;
     public Camera reflectionCamera;
     [SerializeField] private Transform reflectionTransform;
-    float effectNearPlane;
+    public float effectNearPlane=-0.5f;
     void Start(){
         playerCamera = GameObject.FindGameObjectWithTag("PlayerCamera").GetComponent<Camera>();
     }
@@ -17,27 +17,16 @@ public class Portal : MonoBehaviour
 
         Vector3 worldPosition = playerCamera.transform.position;
         Vector3 localPosition =  reflectionTransform.InverseTransformPoint(worldPosition);
+        localPosition.z = -localPosition.z;
         otherPortal.reflectionCamera.transform.position = otherPortal.transform.TransformPoint(localPosition);
+        
 
         Vector3 worldDirection = playerCamera.transform.forward;
         Vector3 localDirection = reflectionTransform.InverseTransformDirection(worldDirection);
+        localDirection.z = -localDirection.z;
         otherPortal.reflectionCamera.transform.forward= otherPortal.transform.TransformDirection(localDirection);
-     /*   // Convert player direction to this portal's local space
-        Vector3 localDir = transform.InverseTransformDirection(playerCamera.transform.forward);
-        localDir.z = -localDir.z;
-
-        // Same for up vector to preserve roll
-        Vector3 localUp = transform.InverseTransformDirection(playerCamera.transform.up);
-        localUp.z = -localUp.z;
-
-        // Apply rotation
-        otherPortal.reflectionCamera.transform.rotation = Quaternion.LookRotation(
-            otherPortal.transform.TransformDirection(localDir),
-            otherPortal.transform.TransformDirection(localUp)
-        );*/
-
-        //float distance = Vector3.Distance( otherPortal.reflectionTransform.transform.position, otherPortal.reflectionTransform.position );
-        //otherPortal.portalCamera.nearClipPlane= Mathf.Max(0.0f,distance)+effectNearPlane;
+     
+        otherPortal.reflectionCamera.nearClipPlane= Mathf.Max(0.0f,distance)+effectNearPlane;
 
 
     }
